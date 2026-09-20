c5be6697488eecfcbdb84e9ce7f8ba32d155f488
sohai <SyntaxTechnologies>
2026-09-20 19:47:23 +0530

    adding the code for new user story

---
+++ src/main/java/org/scopely/Main.java
@@ -0,0 +1,9 @@
+package org.scopely;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("I am writing this code to commit");
+    }
+}
